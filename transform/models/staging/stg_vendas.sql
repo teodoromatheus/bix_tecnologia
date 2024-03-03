0d26e7f9840cf8e19e60e0395afecc4d5215f327
teodoromatheus <matheusteodoro.r@gmail.com>
2024-03-03 16:17:54 -0300

    feat: Tabela Staging de vendas

--- transform/models/staging/stg_vendas.sql
+++ transform/models/staging/stg_vendas.sql
@@ -1,1 +1,10 @@
-{{ config(materialized='table') }}
+with vendas as (
+    select  id_venda
+            , id_funcionario
+            , id_categoria
+            , cast(data_venda as date) as data_venda
+            , venda
+    from {{ source('raw_data','vendas') }}
+)
+
+select * from vendas
