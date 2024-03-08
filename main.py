90e1215e9f6b88a84e4261bc415e218d891b63b6
teodoromatheus <matheusteodoro.r@gmail.com>
2024-03-08 08:54:52 -0300

    feat: Schedule do pipeline

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from extract.PostgreSQL.postgre_collector import PostgreCollector
 from extract.API.api_collector import APICollector
 from extract.ParquetFile.parquet_collector import ParquetFileCollector
+import os
+import schedule
+import time
+import subprocess
 
-PostgreCollector().start()
-APICollector().start()
-ParquetFileCollector().start()
+def run_pipeline():
+    # Executa funções de extração
+    PostgreCollector().start()
+    APICollector().start()
+    ParquetFileCollector().start()
+    time.sleep(5)
+    # Executa o dbt
+    os.chdir('transform')
+    subprocess.run(['dbt','run'])
+
+schedule.every(2).minutes.do(run_pipeline())
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
